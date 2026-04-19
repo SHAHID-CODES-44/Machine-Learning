efd13fe51fa7bd15096a9e15d03a6c4ef0c21bc0
shahid-codes-44 <shahidfirozaba@gmail.com>
2026-04-19 12:02:28 +0530

    Commit

--- Experiment-01/Q1.py
+++
@@ -1,9 +0,0 @@
-# Merging Data Sources – Excel File
-import pandas as pd
-
-df1 = pd.read_excel("../Dataset/students_data.xlsx")
-df2 = pd.read_excel("../Dataset/students_data.xlsx")
-
-merged = pd.merge(df1, df2, on="Student_ID")
-
-print(merged.head())
--- Experiment-01/Q6.py
+++
@@ -1,8 +0,0 @@
-# Feature Correlation
-import pandas as pd
-
-df = pd.read_csv("../Dataset/students_data.csv")
-
-corr = df.corr(numeric_only=True)
-
-print(corr)
